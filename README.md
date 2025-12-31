a6a7b3e8fefc064d1817f5345b696de6dbc55f1d
Simon Thornley <sithor@users.noreply.github.com>
2025-12-31 03:45:07 +0000

    Upload Marzipano

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Parnell_pano
+
+A Marzipano 360 pano in Parnell. Created 31/12/2025.
---
+++ app-files/data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pano_0001-panorama-edit",
+      "name": "PANO_0001 Panorama-Edit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 0.9328564218157798,
+        "pitch": 0.14128582654770128,
+        "fov": 1.4743437640676897
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Lichfield Rd",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
